da84da86bc70be28142ced7277e2d3b98a606035
Jakub Kmiotek <jkmiotek@vewd.com>
2018-04-07 14:41:09 +0200

    initial front

---
+++ ramsey_front/src/components/Game.vue
@@ -0,0 +1,75 @@
+<!--<template>-->
+  <!--<v-container fluid>-->
+    <!--<v-slide-y-transition mode="out-in">-->
+      <!--<v-layout column align-center>-->
+        <!--<img src="@/assets/logo.png" alt="Vuetify.js" class="mb-5">-->
+        <!--<blockquote>-->
+          <!--&#8220;First, solve the problem. Then, write the code.&#8221;-->
+          <!--<footer>-->
+            <!--<small>-->
+              <!--<em>&mdash;John Johnson</em>-->
+            <!--</small>-->
+          <!--</footer>-->
+        <!--</blockquote>-->
+      <!--</v-layout>-->
+    <!--</v-slide-y-transition>-->
+  <!--</v-container>-->
+<!--</template>-->
+
+<template>
+  <v-container>
+    <v-slide-y-transition mode="out-in">
+      <v-layout column align-center>
+        <cytoscape :config="config" style="width: 100%; height: 650px"/>
+      </v-layout>
+    </v-slide-y-transition>
+  </v-container>
+</template>
+
+<script>
+  const config =  {
+    elements: [
+      { // node a
+        data: { id: 'a' }
+      }, { // node b
+        data: { id: 'b' }
+      }, { // edge ab
+        data: { id: 'ab', source: 'a', target: 'b' }
+      }
+    ],
+    style: [
+      {
+        selector: 'node',
+        style: {
+          'background-color': '#666',
+          // 'label': 'data(id)'
+        }
+      },
+      {
+        selector: 'edge',
+        style: {
+          'width': 3,
+          'line-color': '#ccc',
+          'target-arrow-color': '#ccc',
+          'target-arrow-shape': 'triangle'
+        }
+      }
+    ],
+    layout: {
+      name: 'grid',
+      rows: 1
+    }
+  };
+
+  export default {
+    data () {
+      return {
+        config: config,
+      }
+    },
+  }
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+</style>
---
+++ ramsey_front/src/main.js
@@ -0,0 +1,31 @@
+// The Vue build version to load with the `import` command
+// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+import Vue from 'vue'
+import App from './App'
+import router from './router'
+import Vuetify from 'vuetify'
+import VueCytoscape from 'vue-cytoscape'
+
+import 'vuetify/dist/vuetify.min.css'
+import 'vue-cytoscape/dist/vue-cytoscape.css'
+
+Vue.use(Vuetify, { theme: {
+  primary: '#ee44aa',
+  secondary: '#424242',
+  accent: '#82B1FF',
+  error: '#FF5252',
+  info: '#2196F3',
+  success: '#4CAF50',
+  warning: '#FFC107'
+}});
+Vue.use(VueCytoscape);
+
+Vue.config.productionTip = false;
+
+/* eslint-disable no-new */
+new Vue({
+  el: '#app',
+  router,
+  components: { App },
+  template: '<App/>'
+});

3e6806606b3b2ae9d8068b51149adeb62aaaeb10
Jakub Kmiotek <jkmiotek@vewd.com>
2018-06-05 00:09:49 +0200

    Add new game front zoomable scrolable

--- ramsey_front/config/dev.env.js
+++ ramsey_front/config/dev.env.js
@@ -1,9 +1,6 @@
 'use strict'
-const merge = require('webpack-merge')
-const prodEnv = require('./prod.env')
-
-module.exports = merge(prodEnv, {
+module.exports = {
   NODE_ENV: '"development"',
-  SERVER_URl: "'http://0.0.0.0:8000/'",
+  SERVER_URL: "'http://0.0.0.0:8000/'",
   WS_SERVER_URL: "'ws://0.0.0.0:8000/'"
-})
+};
